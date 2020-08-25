bc236229c63e4d0e4717262825756bca9007ba36
mcu8 <jleszczus@gmail.com>
2020-08-25 14:57:20 +0200

    Updated version to 103

--- ModdingTools/BuildData.cs
+++ ModdingTools/BuildData.cs
@@ -4,6 +4,6 @@
     {
         public const string UpdateUrl = "https://raw.githubusercontent.com/mcu8/OpenModManager/master/ModdingTools/version.txt";    
         public const string ReleasesPage = "https://github.com/mcu8/OpenModManager/releases/latest";
-        public const long CurrentVersion = 102;
+        public const long CurrentVersion = 103;
     }
 }
