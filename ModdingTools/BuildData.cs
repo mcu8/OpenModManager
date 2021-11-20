eef76a6231bb5837aa07bb9219cce78085947adb
m_cu8 <jleszczus@gmail.com>
2021-11-20 07:21:49 +0100

    Changed version to b138

--- ModdingTools/BuildData.cs
+++ ModdingTools/BuildData.cs
@@ -4,6 +4,6 @@
     {
         public const string UpdateUrl = "https://hat.ovh/omm_version.txt";    
         public const string ReleasesPage = "https://github.com/mcu8/OpenModManager/releases/latest";
-        public const long CurrentVersion = 137;
+        public const long CurrentVersion = 138;
     }
 }
