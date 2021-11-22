b8d9ab9fbfd4c358757ff5990908b6d99f4b305d
m_cu8 <jleszczus@gmail.com>
2021-11-22 09:12:53 +0100

    Added changelog url

--- ModdingTools/BuildData.cs
+++ ModdingTools/BuildData.cs
@@ -4,6 +4,7 @@
     {
         public const string UpdateUrl = "https://hat.ovh/omm_version.txt";    
         public const string ReleasesPage = "https://github.com/mcu8/OpenModManager/releases/latest";
+        public const string ChangeLogUrl = "https://hat.ovh/omm_changelog.php";
         public const long CurrentVersion = 139;
     }
 }
